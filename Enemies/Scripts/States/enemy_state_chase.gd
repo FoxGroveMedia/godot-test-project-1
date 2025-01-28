e5abc473ec6412d68cb9d9069a94485bd190dde0
Chris Wilson <chris@foxgrovemedia.com>
2025-01-28 10:40:22 -0700

    E48 Bug fixes.

--- Enemies/Scripts/States/enemy_state_chase.gd
+++ Enemies/Scripts/States/enemy_state_chase.gd
@@ -1,5 +1,7 @@
 class_name EnemyStateChase extends EnemyState
 
+const PATHFINDER : PackedScene = preload("res://enemies/pathfinder.tscn")
+
 @export var anim_name : String = "chase"
 @export var chase_speed : float = 40.0
 @export var turn_rate : float = 0.25
@@ -9,6 +11,8 @@ class_name EnemyStateChase extends EnemyState
 @export var attack_area : HurtBox
 @export var state_aggro_duration : float = 0.5
 @export var next_state : EnemyState
+
+var pathfinder : Pathfinder
 
 var _timer : float = 0.0
 var _direction : Vector2
@@ -26,6 +30,9 @@ func init() -> void:
 
 # What happens when the player enters this state?
 func enter() -> void:
+	pathfinder = PATHFINDER.instantiate() as Pathfinder
+	enemy.add_child( pathfinder )
+
 	_timer = state_aggro_duration
 
 	enemy.update_animation( anim_name )
@@ -36,6 +43,7 @@ func enter() -> void:
 
 # What happens when the player exits this state?
 func exit() -> void:
+	pathfinder.queue_free()
 	if attack_area:
 		attack_area.monitoring = false
 	_can_see_player = false
@@ -47,9 +55,9 @@ func process( _delta : float ) -> EnemyState:
 	if PlayerManager.player.hp <= 0:
 		return next_state
 
-	var new_direction : Vector2 = enemy.global_position.direction_to( PlayerManager.player.global_position )
-	_direction = lerp( _direction, new_direction, turn_rate )
+	_direction = lerp( _direction, pathfinder.move_dir, turn_rate )
 	enemy.velocity = _direction * chase_speed
+	
 	if enemy.set_direction( _direction ):
 		enemy.update_animation( anim_name )
 
--- tilemaps/level_titlemap.gd
+++ tilemaps/level_titlemap.gd
@@ -1,5 +1,7 @@
 class_name LevelTitlemap extends TileMapLayer
 
+@export var title_size : float = 32
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,9 +12,9 @@ func _ready():
 func get_titlemap_bounds() -> Array[ Vector2 ]:
 	var bounds : Array[ Vector2 ] = []
 	bounds.append(
-		Vector2( get_used_rect().position * rendering_quadrant_size )
+		Vector2( get_used_rect().position * title_size ) + position
 	)
 	bounds.append(
-		Vector2( get_used_rect().end * rendering_quadrant_size )
+		Vector2( get_used_rect().end * title_size ) + position
 	)
 	return bounds
